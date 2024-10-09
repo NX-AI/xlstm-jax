b6b1fae718b211d3d314e97bbbf22d429f61a47f
Patrick Blies <patrick.blies@nx-ai.com>
2024-10-09 14:59:31 +0200

    Fix bug in test/convtest.py to enable use of GPUs (#120)
    * fix: enable use of gpus by fixing bug in test/convtest.py
    
    * fix: add check for when CUDA_VISIBLE_DEVICES is ""
    
    * chore: add test if JAX_PLATFORM_NAME has been set

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,17 @@ import pytest
 from xlstm_jax.distributed.xla_utils import simulate_CPU_devices
 
 # Select CPU or GPU devices.
-if "CUDA_VISIBLE_DEVICES" not in os.environ:
+if "CUDA_VISIBLE_DEVICES" not in os.environ or os.environ["CUDA_VISIBLE_DEVICES"] == "":
     os.environ["CUDA_VISIBLE_DEVICES"] = ""
-if "JAX_PLATFORMS" not in os.environ:
     os.environ["JAX_PLATFORMS"] = "cpu"
+elif "JAX_PLATFORM_NAME" not in os.environ:
+    # Don't override JAX_PLATFORMS if it has been set.
+
+    # If JAX_PLATFORM_NAME has not been set, set it to auto-congigure (i.e., to "").
+    # Setting it this way will let jax automatically configure the platform, which
+    # defaults to GPU when GPUs are available (which they should be when CUDA_VISIBLE_DEVICES
+    # is set).
+    os.environ["JAX_PLATFORMS"] = ""
 
 # Select number of devices. On CPU, we simulate 8 devices.
 if os.environ["JAX_PLATFORMS"] == "cpu":
