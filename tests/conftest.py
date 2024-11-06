89fdb521396267c4fa7388c0a56231240e1017c3
Phillip Lippe <phillip.lippe@nx-ai.com>
2024-11-06 16:41:47 +0100

    fix: typo in conftest (#234)

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,10 @@ import pytest
 if "CUDA_VISIBLE_DEVICES" not in os.environ or os.environ["CUDA_VISIBLE_DEVICES"] == "":
     os.environ["CUDA_VISIBLE_DEVICES"] = ""
     os.environ["JAX_PLATFORMS"] = "cpu"
-elif "JAX_PLATFORM_NAME" not in os.environ:
+elif "JAX_PLATFORMS" not in os.environ:
     # Don't override JAX_PLATFORMS if it has been set.
 
-    # If JAX_PLATFORM_NAME has not been set, set it to auto-congigure (i.e., to "").
+    # If JAX_PLATFORMS has not been set, set it to auto-congigure (i.e., to "").
     # Setting it this way will let jax automatically configure the platform, which
     # defaults to GPU when GPUs are available (which they should be when CUDA_VISIBLE_DEVICES
     # is set).
